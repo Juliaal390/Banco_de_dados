f06287e1f9d9566b57e68368293fb80c4d9f6288
Julia Alves <juliaa.brito22@gmail.com>
2024-01-21 00:50:31 -0300

    att

--- Biblioteca (exercícios).sql
+++ Biblioteca (exercícios).sql
@@ -2,8 +2,10 @@
     select nome, cpf, dtnascimento from tblalunos where cpf like '123%';
 
 
+
 --listar alunos com data de nascimento menor que 1/10/2020 e data de cadastro maior/igual à 02/06/2008
     select * from tblalunos where dtnascimento<'2020-10-01' and dtcadastro >='2008-06-02';
+
 
 
 --listar livros do autor "autor 1" mostrando o nome do autor, nome do livro e ano de publicação
@@ -14,6 +16,7 @@
     where nomeautor='Autor 1';
 
 
+
 --liste o nome dos alunos e separe suas datas de nascimento em ano, mês e dia
     SELECT
     nome,
@@ -21,6 +24,7 @@
     EXTRACT(MONTH FROM dtnascimento) AS mes,
     EXTRACT(DAY FROM dtnascimento) AS dia
     FROM tblalunos;
+
 
 
 --converta o campo codigo de tblunidadesacervo de CHAR para INT, com autoincremento
